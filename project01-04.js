541c7465a67daf667ba34b26790b8ee02ea05e6c
RocklandStudio <92898229+RocklandStudio@users.noreply.github.com>
2021-10-27 21:07:00 -0500

    hands on week 1

---
+++ project01-04.js
@@ -0,0 +1,41 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Drew McGuinness
+    Date:  10/27/21 
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+
+var homeStreet="1 Main St.";
+
+var homeCity="Sicilia";
+
+var homeState="MA";
+
+var homeCode="02103";
+
+var workStreet="15 Oak Ln.";
+
+var workCity="Central City";
+
+var WorkState="MA";
+
+var workCode="02104";
+<head>
+<script>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<title>Hands-on Project 1-4</title>
+<link rel="stylesheet" href="styles.css" />
+</script>
+
+<script src="project01-04.js"></script>
+
+    
+
+</head>
